50869c8cac7947f7b67bdbbc465082356fb08b3d
rodrigo.baggio.si@gmail.com <Baggio25>
2024-07-31 14:02:11 -0300

    Primeiro teste de renderização

--- dscommerce/src/App.tsx
+++ dscommerce/src/App.tsx
@@ -1,13 +1,62 @@
-import './App.css'
+import computerImg from "./assets/images/computer.png";
+import cartImg from "./assets/images/cart.svg";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <h1>DSCommerce</h1>
+      <header className="dsc-header-client">
+        <nav className="dsc-container">
+          <h1>DSCommerce</h1>
+          <div className="dsc-navbar-right">
+            <div className="dsc-menu-items-container">
+              <div className="dsc-menu-item">
+                <img src={cartImg} alt="carrinho" />
+              </div>
+
+              <a href="#">Entrar</a>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className="dsc-container dsc-product-details-section">
+          <div className="dsc-card dsc-mb-20">
+            <div className="dsc-product-details-top dsc-line-bottom">
+              <img src={computerImg} alt="product" />
+            </div>
+            <div className="dsc-product-details-bottom">
+              <h3>R$ 5.000,00</h3>
+              <h4>Notebook DELL 15,5" 4K 512SSD 8GB</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+                vel aspernatur vitae quod nobis cumque placeat id, excepturi
+                corporis sit maxime libero nemo atque! Ad, ratione eos.
+                Consequatur porro distinctio debitis facere ex molestias
+                quibusdam quos eveniet ratione, totam perspiciatis aliquam culpa
+                asperiores tempora soluta aperiam quia commodi magni velit harum
+                fugit eius nemo assumenda? Sed reiciendis blanditiis velit atque
+                eius eveniet illo hic ipsum reprehenderit magnam earum eos
+                dolorem cupiditate, eaque voluptate doloremque tenetur pariatur
+                veritatis illum qui commodi similique necessitatibus aliquam!
+                Facilis rerum beatae a voluptas adipisci nemo non voluptatibus
+                cum.
+              </p>
+
+              <div className="dsc-category-container">
+                <div className="dsc-category">Eletônicos</div>
+                <div className="dsc-category">Computadores</div>
+              </div>
+            </div>
+          </div>
+          <div className="dsc-btn-page-container">
+            <div className="dsc-btn dsc-btn-blue">Comprar</div>
+            <div className="dsc-btn dsc-btn-white">Início</div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
