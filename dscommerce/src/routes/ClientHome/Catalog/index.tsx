49eceedd6477fece51d0a1237d38370b50005dc7
rodrigo.baggio.si@gmail.com <Baggio25>
2024-08-12 15:03:49 -0300

    Buscando a próxima página de produtos

--- dscommerce/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce/src/routes/ClientHome/Catalog/index.tsx
@@ -16,6 +16,7 @@ type QueryParams = {
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [isLastPage, setIsLastPage] = useState(false);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
     page: 0,
@@ -26,12 +27,19 @@ export default function Catalog() {
     productService
       .findPageRequest(queryParams.page, queryParams.name)
       .then((response) => {
-        setProducts(response.data.content);
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
       });
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({ ...queryParams, name: searchText });
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
   return (
@@ -45,7 +53,11 @@ export default function Catalog() {
           ))}
         </div>
 
-        <ButtonNextPage />
+        {!isLastPage && (
+          <div onClick={handleNextPageClick}>
+            <ButtonNextPage />
+          </div>
+        )}
       </section>
     </main>
   );
