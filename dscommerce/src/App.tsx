9b0c6e7adbe3fa735e827ac554508c6a7b2e3e7a
rodrigo.baggio.si@gmail.com <Baggio25>
2024-08-13 13:42:38 -0300

    Criando e provendo o estado global com a Context API

--- dscommerce/src/App.tsx
+++ dscommerce/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import ClientHome from "./routes/ClientHome";
@@ -5,20 +6,30 @@ import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Catalog from "./routes/ClientHome/Catalog";
 import Cart from "./routes/ClientHome/Cart";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
-          <Route index element={<Catalog />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details/:productId" element={<ProductDetails />} />
-          <Route path="cart" element={<Cart />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+import { ContextCartCount } from "./utils/context-cart";
 
+function App() {
+  const [contextCartCount, setContextCartCount] = useState<number>(0);
+
+  return (
+    <ContextCartCount.Provider
+      value={{ contextCartCount, setContextCartCount }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextCartCount.Provider>
   );
 }
 
