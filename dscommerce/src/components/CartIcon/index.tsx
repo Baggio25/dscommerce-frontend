60b9a50336b08fce135dd246f0d179590bfef225
rodrigo.baggio.si@gmail.com <Baggio25>
2024-08-13 08:08:56 -0300

    Buscando os dados do localStorage

--- dscommerce/src/components/CartIcon/index.tsx
+++ dscommerce/src/components/CartIcon/index.tsx
@@ -1,13 +1,19 @@
 
+import { useState } from "react";
 import cartIcon from "../../assets/images/cart.svg";
+
+import * as cartService from "../../services/cart-service";
 
 import "./styles.css";
 
 export default function CartIcon() {
+
+  const [cart, setCart] = useState(cartService.getCart());
+
   return (
     <>
       <img src={cartIcon} alt="carrinho" />
-      <div className="dsc-cart-count">2</div>
+      <div className="dsc-cart-count">{cart.items.length}</div>
     </>
   );
 }
