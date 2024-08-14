af7b29e628e0c853e7fc394941e7b6d71b487ef0
rodrigo.baggio.si@gmail.com <Baggio25>
2024-08-14 13:55:15 -0300

    Buscando o usuário logado do back end

--- dscommerce/src/routes/Admin/Dashboard/index.tsx
+++ dscommerce/src/routes/Admin/Dashboard/index.tsx
@@ -1,12 +1,31 @@
 
+import { useEffect, useState } from "react";
+
+import { UserDTO } from "../../../models/user";
+
+import * as userService from "../../../services/user-service";
+
 import "./styles.css";
 
 export default function Dashboard() {
+
+  const [user, setUser] = useState<UserDTO>();
+
+  useEffect(() => {
+    userService.findMe()
+      .then(response => {
+        setUser(response.data);
+      })
+      .catch(error => {
+        console.log("Erro de busca do usuários - ", error);
+      });
+  }, []);
+
   return (
     <main>
       <section className="dsc-container dsc-admin-section">
         <h2 className="dsc-section-title dsc-mb-20">Dashboard</h2>
-        <h4 className="dsc-section-subtitle dsc-mb-20">Bem Vindo - </h4>
+        <h4 className="dsc-section-subtitle dsc-mb-20">Bem Vindo - {user?.name}</h4>
       </section>
     </main>
   );
--- dscommerce/src/routes/ClientHome/Login/index.tsx
+++ dscommerce/src/routes/ClientHome/Login/index.tsx
@@ -17,7 +17,7 @@ export default function Login() {
     event.preventDefault();
     authService.loginRequest(formData)
       .then(response => {
-        console.log(response.data)
+        authService.saveAccessToken(response.data.access_token);
       })
       .catch(error => {
         console.log("Erro no login ", error)
