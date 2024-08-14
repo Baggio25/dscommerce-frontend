aee123820a9d90bfb7e8723a6a3e9701d13169c7
rodrigo.baggio.si@gmail.com <Baggio25>
2024-08-14 14:22:49 -0300

    Interceptors do Axios

--- dscommerce/src/routes/Admin/Dashboard/index.tsx
+++ dscommerce/src/routes/Admin/Dashboard/index.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 
 import { UserDTO } from "../../../models/user";
@@ -8,24 +7,23 @@ import * as userService from "../../../services/user-service";
 import "./styles.css";
 
 export default function Dashboard() {
-
   const [user, setUser] = useState<UserDTO>();
 
   useEffect(() => {
-    userService.findMe()
-      .then(response => {
+    userService
+      .findMe()
+      .then((response) => {
         setUser(response.data);
       })
-      .catch(error => {
-        console.log("Erro de busca do usuários - ", error);
-      });
   }, []);
 
   return (
     <main>
       <section className="dsc-container dsc-admin-section">
         <h2 className="dsc-section-title dsc-mb-20">Dashboard</h2>
-        <h4 className="dsc-section-subtitle dsc-mb-20">Bem Vindo - {user?.name}</h4>
+        <h4 className="dsc-section-subtitle dsc-mb-20">
+          Bem Vindo - {user?.name}
+        </h4>
       </section>
     </main>
   );
